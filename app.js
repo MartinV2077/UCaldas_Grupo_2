bf611d9fac8cfb0b2bf782e64b89e96c75bf9b36
Martin Vasquez <martinvz2077@gmail.com>
2021-07-30 16:41:30 -0500

    Tarea creación de Slots - Sprint_3

--- app.js
+++ app.js
@@ -47,6 +47,9 @@ template: `
     <input type="number" v-model="value">
       <span>{{ convertedValue }}</span>
 
+      <slot name="text"></slot>
+      <slot name="link"></slot>
+
       <ul v-show=showPrices>
       <li
         class="uppercase"
