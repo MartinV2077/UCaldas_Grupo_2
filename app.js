42db0be396b8e012c07979853ef72695a04c2c47
Martin Vasquez <martinvz2077@gmail.com>
2021-08-10 15:20:31 -0500

    Tarea renderizado listas-Sp2

--- app.js
+++ app.js
@@ -12,7 +12,23 @@ new Vue({
         img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
 //Propiedad nueva, tarea control flujo con directivas, Sp2
         changePercent: -10,
-        
+//Array plano, propiedad nueva, tarea renderizado listas, precio Bitcoin,  Sp2
+        prices:[8400, 7900, 8200, 9000, 9400, 10000, 10200],
+//Array de objetos con propiedades day y value, tarea renderizado listas, precio Bitcoin,  Sp2
+      pricesWithDays:[
+        { day: 'Lunes', value: "8400" },
+        { day: 'Martes', value: "7900" },
+        { day: 'Miercoles', value: "8200" },
+        { day: 'Jueves', value: "9000" },
+        { day: 'Viernes', value: "9400" },
+        { day: 'Sabado', value: "10000" },
+        { day: 'Domingo', value: "10200" },
+
+      ]
+
     }
   }
-})
+
+}) 
+
+   
