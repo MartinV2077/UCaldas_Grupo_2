bd8cb84031a5c0d1bc3be8248be17bd0a5c81bbf
Martin Vasquez <martinvz2077@gmail.com>
2021-08-10 14:53:35 -0500

    tarea control flujo con directivas, Sp2

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 //Creacion Atributos dinamicos Sprint_1
 
-//Instancia de VUE
+//Instancia de VUE Sp1
 new Vue({
-//Propiedad
+//Propiedad Sp1
   el: '#app',
-//Funcion data que devuelve objeto return
+//Funcion data que devuelve objeto return Sp1
   data () {
     return {
-//Propiedades
+//Propiedades Sp1
         name: 'Bitcoin',
-        img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png'
+        img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+//Propiedad nueva, tarea control flujo con directivas, Sp2
+        changePercent: -10,
+        
     }
   }
 })
