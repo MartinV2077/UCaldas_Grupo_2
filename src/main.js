f28c56805daa662e7904d4580b6bd75a8cd24972
Martin Vasquez <martinvz2077@gmail.com>
2021-08-12 17:44:18 -0500

    Tarea Construccion de Setup VUE Router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css";
+
+import router from '@/router'
 //import de tailwind
 Vue.config.productionTip = false;
 //Instancia de Vue
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount("#app");
