150d2a24b56d20a9e52db28d4f6ee506314b638e
Martin Vasquez <martinvz2077@gmail.com>
2021-08-14 07:46:57 -0500

    tarea Creacion rutas dinamicas - Sprint 5

--- src/api.js
+++ src/api.js
@@ -7,7 +7,29 @@ function getAssets() {
     .then((res) => res.json())
     .then((res) => res.data);
 }
+
+//Ruta dinamica
+function getAsset(coin) {
+  return fetch(`${url}/assets/${coin}`)
+    .then((res) => res.json())
+    .then((res) => res.data);
+}
+
+function getAssetHistory(coin) {
+  const now = new Date();
+  const end = now.getTime();
+  now.setDate(now.getDate() - 1);
+  const start = now.getTime();
+  return fetch(
+    `${url}/assets/${coin}/history?interval=h1&start=${start}&end=${end}`
+  )
+    .then((res) => res.json())
+    .then((res) => res.data);
+}
+
 //comunica componente Home
 export default {
   getAssets,
+  getAsset,
+  getAssetHistory,
 };
