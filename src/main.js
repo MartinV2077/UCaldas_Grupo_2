b4d851ed4b104c78d6da9373dafb0122b658c69b
Martin Vasquez <martinvz2077@gmail.com>
2021-08-13 21:07:15 -0500

    Tarea Mejorar UI con filtros - Sprint 5

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from "./App.vue";
 import "@/assets/css/tailwind.css";
 
 import router from "@/router";
+import { dollarFilter, percentFilter } from "@/filters";
+
 //import de tailwind
 Vue.config.productionTip = false;
+Vue.filter("dollar", dollarFilter);
+Vue.filter("percent", percentFilter);
+
 //Instancia de Vue
 new Vue({
   router,
